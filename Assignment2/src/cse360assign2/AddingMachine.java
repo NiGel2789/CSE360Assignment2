bcd3b777d417191f05cc1812f17c4206d9d34faa
poken <poken@DESKTOP-8TJFDI6>
2019-10-07 00:04:12 -0700

    Initial commit of Assignment2.

---
+++ Assignment2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,54 @@
+package cse360assign2;
+
+/**
+ * A class called AddingMachine
+ */
+public class AddingMachine {
+
+	/**
+	 * The total sum of numbers after calculations.
+	 */
+	private int total;
+	
+	/**
+	 * A method to initialize the total variable to 0.
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * A method to return the total sum.
+	 * @return 0
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * A method that adds a value parameter to the total variable.
+	 * @param value An integer to be added to the total variable.
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * A method that subtracts a value parameter from the total variable.
+	 * @param value An integer to be subtracted from the total variable.
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	public String toString () {
+		return "";
+	}
+
+	/**
+	 * A method to clear.
+	 */
+	public void clear() {
+	
+	}
+}
